b3742cb7b3bfb8d3a41c3eb5a7af3f1ebebc3552
Efrice Antice <macum98@gmail.com>
2021-10-25 21:24:56 -0700

    Add project files.

---
+++ Homework Week 9/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Week_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer tripA = new Odometer();
+            tripA.Add(100,6);
+            tripA.Add(14, 1);
+            Console.WriteLine(tripA.ToString());
+            Odometer tripB = new Odometer(11,2);
+            tripB.Add(35,3);
+            tripB.Add(28,3);
+            Console.WriteLine(tripB.ToString());
+            tripA.Reset();
+            tripA.Add(312, 12);
+            Console.WriteLine(tripA.ToString());
+            Console.Read();
+        }
+    }
+
+    class Odometer
+    {
+        int miles_driven;
+        double fuel_used;
+
+        public Odometer()
+        {
+            miles_driven = 0;
+            fuel_used = 0;
+        }
+
+        public Odometer(int miles_gone, double gas_used)
+        {
+            miles_driven = miles_gone;
+            fuel_used = gas_used;
+        }
+
+        public void Reset()
+        {
+            miles_driven = 0;
+            fuel_used = 0;
+        }
+
+        public void Add(int add_miles, double add_fuel)
+        {
+            if (add_miles < 0 || add_fuel < 0)
+                return;
+
+            miles_driven += add_miles;
+            fuel_used += add_fuel;
+        }
+
+        public double GetMilesPerGallon()
+        {
+            return miles_driven / fuel_used;
+        }
+
+        public override string ToString()
+        {
+            return "You have driven " + miles_driven + " miles, using " + fuel_used + " gallons of gas, meaning you got " + GetMilesPerGallon() + " miles per gallon.";
+        }
+    }
+}
---
+++ HwPart2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HwPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp1 = new Temperature();
+            Console.WriteLine(temp1.ToString());
+
+            Temperature temp2 = new Temperature('C', 20);
+            temp2.SetTemperature(45);
+            Console.WriteLine(temp2.ToString());
+            temp2.SetScaleAndTemperature('F', 32);
+
+            if (temp1.Equals(temp2))
+            {
+                Temperature temp3 = new Temperature('F');
+                temp3.SetTemperature(84);
+                Console.WriteLine(temp3.ToString());
+            }
+            else
+            {
+                Temperature temp4 = new Temperature(40);
+                temp4.SetScale('F');
+                Console.WriteLine(temp4.ToString());
+            }
+
+            Console.Read();
+        }
+    }
+
+    class Temperature
+    {
+        float temperature;
+        char scale;
+
+        public Temperature()
+        {
+            temperature = 0;
+            scale = 'C';
+        }
+
+        public Temperature(float _temperature)
+        {
+            scale = 'C';
+            temperature = _temperature;
+        }
+
+        public Temperature(char _scale)
+        {
+            scale = char.ToUpper(_scale);
+            if (_scale != 'C' && _scale != 'F')
+                scale = 'C';
+            temperature = 0;
+        }
+
+        public Temperature(char _scale, float _temperature)
+        {
+            scale = char.ToUpper(_scale);
+            if (_scale != 'C' && _scale != 'F')
+                scale = 'C';
+
+            temperature = _temperature;
+        }
+
+        public void SetTemperature(float _temp)
+        {
+            temperature = _temp;
+        }
+
+        public void SetScale(char _scale)
+        {
+            scale = char.ToUpper(_scale);
+            if (_scale != 'C' && _scale != 'F')
+                scale = 'C';
+        }
+
+        public void SetScaleAndTemperature(char _scale, float _temp)
+        {
+            scale = char.ToUpper(_scale);
+            if (_scale != 'C' && _scale != 'F')
+                scale = 'C';
+
+            temperature = _temp;
+        }
+
+        public float GetTemperatureCelsius()
+        {
+            if (scale != 'C')
+                return (temperature - 32) / 1.8f;
+            return temperature;
+        }
+
+        public float GetTemperatureFahrenheit()
+        {
+            if (scale != 'F')
+                return temperature * 1.8f + 32;
+            return temperature;
+        }
+
+        public bool Equals(Temperature _temperature)
+        {
+            return GetTemperatureFahrenheit() == _temperature.GetTemperatureFahrenheit();
+        }
+
+        public override string ToString()
+        {
+            if (scale == 'C')
+                return temperature + " degrees Celsius";
+            return temperature + " degrees Fahrenheit";
+        }
+    }
+}
